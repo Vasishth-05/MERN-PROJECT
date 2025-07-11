cede05c0cf287ad6de31e308751596166e6261d5
Vasishth <learnvasishth@gmail.com>
2025-07-11 15:47:37 +0530

    added server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send({
+        message: 'successfull'
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+})
